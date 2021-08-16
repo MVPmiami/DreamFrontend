2766626211d65b9532e43c3fd85b6ca85a777d02
Aliaksei Siankevich <andfly@tut.by>
2021-08-16 16:56:56 +0300

    init: init webpack-react

---
+++ src/components/app.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './app.scss'
+
+const App = () => {
+  return (
+    <div className='test-style'>
+      React is working !
+    </div>
+  )
+}
+
+export default App;
+
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HTMLWebpackPligin = require('html-webpack-plugin');
+const { Template } = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: ["@babel/polyfill", './src/index.jsx'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    open: true,
+    port: 9000,
+  },
+  plugins: [
+    new HTMLWebpackPligin({template: './src/index.html'}),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg)/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+        loader: "babel-loader",
+        options: {
+        presets: ['@babel/preset-env']
+            }
+          }
+      },
+      {
+        test: /\.m?jsx$/,
+        exclude: /node_modules/,
+        use: {
+        loader: "babel-loader",
+        options: {
+                presets: ["@babel/preset-react", '@babel/preset-env']
+                }
+        }
+      }
+    ]
+  }
+}
